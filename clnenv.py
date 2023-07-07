0ccac036fcf13cf9e333f492ec81fe049949dd94
andrew <andrey.resler@yandex.ru>
2023-07-07 23:00:18 +0200

    Some improvements

--- clnenv.py
+++ clnenv.py
@@ -7,37 +7,55 @@ from pathlib import Path
 from shutil import rmtree
 from subprocess import Popen, PIPE, STDOUT
 
+small_sep = '-----'
 
-def clean_dir(target_dir):
-    iterdir = Path(target_dir).iterdir()
-    if any(iterdir):
-        print(f'{target_dir}:')
-        for i in iterdir:
+
+def clean_dir(target_dir, name):
+    t = Path(target_dir)
+    print(f'{name}\n{small_sep}\n')
+    if any(t.iterdir()):
+        print(f'{target_dir}:\n')
+        for i in t.iterdir():
             print(i.name)
 
-        inpt = input('Do you want to clean this folder? Y/N: ')
+        inpt = input('\nDo you want to clean this folder? Y/N: ')
 
         if inpt in ['y', 'Y']:
-            for i in Path(target_dir).iterdir():
+            for i in t.iterdir():
                 print(f'Removing {i}')
                 if i.is_dir():
                     rmtree(i)
                 else:
                     i.unlink()
+            print('')
         else:
-            print('Skipping...')
+            print(f'Skipping...\n')
             return 0
     else:
-        print(f'"{target_dir}" is empty...')
+        print(f'{target_dir} is empty...\n')
 
 
 def clean_conda(conda_bin):
     sub = Popen([conda_bin, 'env', 'list', '--json'], stdout=PIPE)
-    envs = json.loads(sub.stdout.read())
+    envs = json.loads(sub.stdout.read())['envs']
 
-    for env in envs['envs'][1:]:
-        s = Popen([conda_bin, 'env', 'remove', '-p', env], stdout=PIPE, stderr=STDOUT)
-        print(s.stdout.read())
+    print(f'conda\n{small_sep}\n')
+    if len(envs) > 1:  # Supposedly first element is always a base env
+        print(f'Found conda envs: \n')
+        for env in envs[1:]:
+            print(env)
+
+        inpt = input('\nRemove all? Y/N: ')
+        if inpt in ['y', 'Y']:
+            for env in envs[1:]:
+                s = Popen([conda_bin, 'env', 'remove', '-p', env], stdout=PIPE, stderr=STDOUT)
+                print(s.stdout.read())
+            print('')
+        else:
+            print('Skipping...\n')
+            return 0
+    else:
+        print(f'No conda envs found...\n')
 
 
 def main():
@@ -60,13 +78,13 @@ def main():
         case 'conda':
             clean_conda(config['bin']['conda'])
         case 'poetry':
-            clean_dir(config['env']['poetry'])
+            clean_dir(config['env']['poetry'], 'poetry')
         case 'pipenv':
-            clean_dir(config['env']['pipenv'])
+            clean_dir(config['env']['pipenv'], 'pipenv')
         case 'all':
             clean_conda(config['bin']['conda'])
-            clean_dir(config['env']['poetry'])
-            clean_dir(config['env']['pipenv'])
+            clean_dir(config['env']['poetry'], 'poetry')
+            clean_dir(config['env']['pipenv'], 'pipenv')
 
 
 if __name__ == '__main__':
